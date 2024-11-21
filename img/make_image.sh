ed0bbb239a49002a49d99597a395651c3585b0c9
Allie Fitter <fitterj@gmail.com>
2024-11-21 15:33:10 -0600

    More work on install

--- img/make_image.sh
+++ img/make_image.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 export DIR="$(dirname "$(realpath "$0")")"
+echo "$USER" > SSH_USER
 sudo sdm --customize \
-  --plugin user:"adduser=$USER|password=$PASSWORD" \
+  --plugin user:"adduser=$USER|password=$PASS" \
+  --plugin apps:"apps=@$DIR/apps" \
   --plugin L10n:host \
   --plugin disables:piwiz \
-  --plugin sshkey:"sshuser=$USER|keyname=calvinpi|import-key=$SSH_KEY|authkey=true" \
   --plugin network:"nmconn=$DIR/wifi.nmconnection" \
   --plugin runatboot:"script=$DIR/first_boot.sh|output=/var/log/calvin.first_boot.log|error=/var/log/calvin.first_boot.log" \
   --plugin copyfile:"from=$COMICS_ZIP_PATH|to=/usr/share/calvin/|mkdirif=true" \
+  --plugin copyfile:"from=$PUBLIC_KEY_PATH|to=/home/$USER/.ssh/|mkdirif=true" \
+  --plugin copyfile:"from=$DIR/SSH_USER|to=/usr/share/calvin/|mkdirif=true" \
   --extend --xmb 6144 \
   --expand-root \
   --regen-ssh-host-keys \
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,17 +2,18 @@
 
 export POETRY=/root/.local/bin/poetry
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+export COMICS_PATH=/usr/share/calvin/comics/
+export SSH_USER=$(</usr/share/calvin/SSH_USER)
+mv /home/$SSH_USER/.ssh/calvin.pub /home/$SSH_USER/.ssh/authorized_keys
+chown -R $SSH_USER:$SSH_USER /home/$SSH_USER/.ssh/
 mkdir /tmp/calvin_and_hobbes
 cd /tmp/calvin_and_hobbes
 dphys-swapfile swapoff
 sed -ie 's/CONF_SWAPSIZE=.*$/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
-apt update
-apt install git nodejs npm nginx libjpeg-dev zlib1g-dev python3-virtualenv lightdm openbox python3-tk -y
 git clone https://github.com/alliefitter/calvin_and_hobbes_viewer.git
 cd calvin_and_hobbes_viewer
-raspi-config nonint do_boot_behaviour B4
 curl -sSL https://install.python-poetry.org | python3 -
 $POETRY build
 cd hobbes
@@ -26,13 +27,16 @@ mkdir /app/calvin/comics
 mkdir /app/hobbes
 cp dist/*.whl /app/calvin
 cp -r hobbes/dist/* /app/hobbes
+cp etc/.xsession /home/$SSH_USER/
 cp etc/nginx/* /etc/nginx/conf.d
 cp etc/systemd/* /etc/systemd/system/
 cp scripts/xhost_calvin.sh /usr/bin/xhost-calvin
+chmod +x /usr/bin/xhost-calvin
 cd /app/calvin
 virtualenv venv
 ./venv/bin/pip3 install *.whl
-unzip /usr/share/calvin/comics.zip -d /usr/share/calvin/comics/
+./venv/bin/calvin init-db
+unzip /usr/share/calvin/comics.zip -d "$COMICS_PATH"
 rm /usr/share/calvin/comics.zip
 sed -ie 's/user .*$/user hobbes hobbes/g' /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/*
@@ -45,3 +49,11 @@ systemctl enable calvin-daemon.service
 systemctl enable calvin-daily.service
 systemctl enable calvin-daily.timer
 systemctl enable calvin-xhost.service
+(cat >> /etc/lightdm/lightdm.conf)
+raspi-config nonint do_boot_behaviour B4
+(cat | tee -a /etc/lightdm/lightdm.conf  >/dev/null) << EOF
+[SeatDefaults]
+user-session=openbox
+autologin-user=$SSH_USER
+autologin-user-timeout=0
+EOF
